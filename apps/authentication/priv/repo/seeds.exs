5f18eaaecb3d69812c8fd03014d542c088b7f4aa
Dmitriy <xperiall031@gmail.com>
2018-07-04 00:21:07 +0300

    added fake data

--- apps/authentication/priv/repo/seeds.exs
+++ apps/authentication/priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Authentication.Repo
+alias Authentication.User
+
+for _ <- 0..10000 do
+  Repo.insert!(%User{
+    login: Faker.Internet.user_name(),
+    email: Faker.Internet.email(),
+    first_name: Faker.Name.first_name(),
+    last_name: Faker.Name.last_name()
+  })
+end
